d007d7cf35e35caaa3d03c599a70717dbbd44d96
Hansen Emanuel <hansenemanuel@hotmail.com>
2021-09-06 18:12:48 +0700

    Add files via upload

---
+++ Project-Files/Gameplay/Spawn.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	spawn = spawn.instance()
+	add_child(spawn)
+	
